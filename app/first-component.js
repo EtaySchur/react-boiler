bcb78d18e603bb149a1f2a5e8e613430c7f2c7c3
Etay Schur <etay.s@crossrider.com>
2016-06-05 12:47:57 +0300

    First commit

---
+++ app/first-component.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const FirstComponent = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <h1> Fuad ! </h1>
+                           <div>Hello World</div>
+
+                </div>
+        )
+    }
+});
+
+export default FirstComponent;
